2ff00971c6f082303d638c7a8606802c8f20d4a8
ankesankit <Canadajob@123>
2023-01-05 21:24:59 -0500

    Credit risk consume microservice

---
+++ credirisk/src/main/java/com/triad/credirisk/controllers/CreditRiskController.java
@@ -0,0 +1,16 @@
+package com.triad.credirisk.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreditRiskController {
+
+
+    @GetMapping("v1/consume/messages")
+    public void consumeCreditRiskMessages(){
+
+
+    }
+
+}
